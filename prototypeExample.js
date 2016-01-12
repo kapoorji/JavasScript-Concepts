9b2e62addf75d8cab1d7bbfdf3e0b2b7d61bdc72
Himanshu Kapoor <himanshu.kapoor2509@gmail.com>
2016-01-12 23:03:35 +0530

    Proto and prototype example

---
+++ prototypeExample.js
@@ -0,0 +1,12 @@
+// every function has a prototype because JS doesn't which you want to inherivar t
+this.hey = function() {
+var a = 5;
+this.name = "JOE"; // it will visible outside ,so it will become property of newly created obj i.e obj
+return this.name;
+}
+
+// prototype refers function himself 
+// in prototype we have an constructor and a proto
+
+var obj = new hey(); // hey prototype will become it's proto which is hey function and proto object
+
